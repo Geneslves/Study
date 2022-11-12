588b147e8a1dfbfb2533919ad44eb94e54c9bee7
Geneslves <15253770303@163.com>
2022-11-12 15:57:51 +0800

    动态内存函数calloc

---
+++ c/test_22/2211/test_2211_12/2_test.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+#include "errno.h"
+#include "string.h"
+#include "stdlib.h"
+
+
+int main() {
+	int* p = (int*)calloc(10, sizeof(int));
+	if (NULL == p)
+	{
+		printf("%s\n", strerror(errno));
+	}
+	else {
+		int i = 0;
+		for (i = 0; i < 10; i++)
+		{
+			printf("%d ", *(p + i));
+		}
+	}
+
+
+	free(p);
+	p = NULL;
+	return 0;
+
+}
