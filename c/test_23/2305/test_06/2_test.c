4204cf69523082bdeb9a58130ac5ba2f84387e5f
Geneslves <15253770303@163.com>
2023-05-07 00:00:49 +0800

    使用strcpy对name赋值

--- c/test_23/2305/test_06/2_test.c
+++ c/test_23/2305/test_06/2_test.c
@@ -27,11 +27,11 @@ int main()
     SqList *head = (SqList *)malloc(sizeof(SqList));
     test(head);
     printf("%d",head->elem->id);
-} 
+}
 */
-
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 
 #define Book struct book
 
@@ -48,9 +48,10 @@ typedef struct
     int length;
 } SqList;
 
-SqList* test(SqList *head)
+SqList *test(SqList *head)
 {
     head->elem.id = 1;
+    strcpy(head->elem.name, "Book Name"); // 在这里对name赋值
     return head;
 }
 
@@ -58,7 +59,9 @@ int main()
 {
     SqList *head = (SqList *)malloc(sizeof(SqList));
     test(head);
-    printf("%d", head->elem.id);
+    printf("%d\n", head->elem.id);
+    printf("%s\n", head->elem.name); // 在这里打印name
+    free(head);                      // 释放动态分配的内存
     return 0;
 }
 
@@ -106,4 +109,57 @@ int main()
 ```
 
 修改后的代码应该能够正常编译和执行，并输出 `1`。请注意，在使用完动态分配的内存后，应该使用 `free` 函数来释放它。在这个例子中，在 `main` 函数的最后，可以加上 `free(head)` 来释放动态分配的内存。
+*/
+
+/*
+要对`name`进行赋值，可以使用字符串相关的函数，例如`strcpy`函数。以下是修改后的代码示例：
+
+```c
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+#define Book struct book
+
+struct book
+{
+    int id;
+    char name[50];
+    int price;
+};
+
+typedef struct
+{
+    Book elem;
+    int length;
+} SqList;
+
+SqList* test(SqList *head)
+{
+    head->elem.id = 1;
+    strcpy(head->elem.name, "Book Name"); // 使用strcpy函数对name赋值
+    return head;
+}
+
+int main()
+{
+    SqList *head = (SqList *)malloc(sizeof(SqList));
+    test(head);
+    printf("%d\n", head->elem.id);
+    printf("%s\n", head->elem.name); // 打印name
+    free(head); // 释放动态分配的内存
+    return 0;
+}
+```
+
+在`test`函数中，使用`strcpy`函数将字符串 `"Book Name"` 复制到 `head->elem.name` 中，实现对 `name` 的赋值。在`main`函数中，通过 `printf` 函数打印 `name` 的值。请注意，为了使用 `strcpy` 函数，需要包含头文件 `string.h`。
+
+输出结果应该为：
+
+```
+1
+Book Name
+```
+
+注意：在使用 `strcpy` 函数时，要确保目标数组 `name` 足够大，以容纳要复制的字符串，避免造成缓冲区溢出。
 */
